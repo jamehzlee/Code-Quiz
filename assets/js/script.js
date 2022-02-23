1a926c53ab97651baaa965c4f5229e01c129331c
Jamehzz <probableeme@gmail.com>
2022-02-23 18:11:18 -0500

    updated layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,51 @@
 var timer;
 var timeLeft = 60;
 
-var timeLeftEl = document.querySelector(".time-left");
+var a1 = $("#a1");
+var a2 = $("#a2");
+var a3 = $("#a3");
+var a4 = $("#a4");
+var question = false;
+
+
+var timeLeftEl = $("#time-left");
+// var highscore = document.getElementById("highscore").hidden;
+// highscore.hidden = true;
+
 
 function startTimer() {
     timer = setInterval(function() {
-        timeLeft--;
-        timeLeftEl.textContext = timeLeft;
-
-        // if (timeLeft < 0) {
-        //     loseGame();
-        // }
+        if (timeLeft > 0) {
+            timeLeft--;
+            timeLeftEl.text(timeLeft);
+            console.log(timeLeft);
+        }
+        else {
+            timeLeftEl.text("You lose")
+            clearInterval(timer);
+        }
     }, 1000);
 }
 
+function userAnswer() {
+    if (question == true) {
+        // correct();
+        // nextQuestion();
+    }
+    else {
+        if(timeLeft > 0){
+            timeLeft -= 10;
+            console.log(timeLeft);
+        }
+        else {
+            gameOver();
+        }
+        // wrong();
+    }
+}
+function viewHighScore() {
+    
+}
+
+a1.click(userAnswer);
 startTimer();
