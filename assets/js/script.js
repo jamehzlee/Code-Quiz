47312d75fde5ba903a2b7faa944f02a0052a68e3
Jamehzz <probableeme@gmail.com>
2022-02-24 08:11:03 -0500

    timer and wrong answer time penalty

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,41 @@
-var timer;
+var quizArray = [
+    {
+        "question1" : "Commonly used data types DO NOT include",
+        "answers" : ["numbers","string", "boolean", "alert"]
+    },
+    {
+        "question2" : "How to write and IF statement in JavaScript",
+        "answers" : ["if i = 0 then", "if(i == 0)", "if i = 0", "if i == 0 then"],
+
+    },  
+    {
+        "question3" : "Which event occurs when the user clicks on an HTML element",
+        "answers" : ["onchange", "onmouseover", "onclick", "onmouseclick"]
+    },
+    {
+        "question4" : "Which operator is used to set a variable",
+        "answers" : ["==", "-", "=", "--"]
+    }  
+] 
+
+var timeLeftEl = $("#time-left");
 var timeLeft = 60;
+var timer;
 
 var a1 = $("#a1");
 var a2 = $("#a2");
 var a3 = $("#a3");
 var a4 = $("#a4");
-var question = false;
 
+function firstQ() {
+    a1.text(quizArray[0].answers[0])
+    a2.text(quizArray[0].answers[1])
+    a3.text(quizArray[0].answers[2])
+    a4.text(quizArray[0].answers[3])
 
-var timeLeftEl = $("#time-left");
-// var highscore = document.getElementById("highscore").hidden;
-// highscore.hidden = true;
+    
+}
+
 
 
 function startTimer() {
@@ -18,7 +43,6 @@ function startTimer() {
         if (timeLeft > 0) {
             timeLeft--;
             timeLeftEl.text(timeLeft);
-            console.log(timeLeft);
         }
         else {
             timeLeftEl.text("You lose")
@@ -28,24 +52,29 @@ function startTimer() {
 }
 
 function userAnswer() {
-    if (question == true) {
-        // correct();
-        // nextQuestion();
+    if (click() === quizArray[0].answers[3]) {
+        timeLeft = 100;
     }
     else {
         if(timeLeft > 0){
             timeLeft -= 10;
-            console.log(timeLeft);
         }
         else {
-            gameOver();
+            // gameOver();
         }
         // wrong();
     }
 }
-function viewHighScore() {
-    
-}
 
-a1.click(userAnswer);
-startTimer();
+
+function run() {
+    startTimer();
+    firstQ();
+    userAnswer();
+}
+run();
+// a2.click(userAnswer);
+
+// a3.click(userAnswer);
+
+// a4.click(userAnswer);
